5bd97713b138fe6bc04cd1dad43c8940ac77c58a
Ronald Martinez <rmartinez@integralplm.com>
2021-06-18 14:41:21 -0500

    init project of very simple proxy

---
+++ src/main/java/com/miniproxy/proxyserver/ProxyServerApplication.java
@@ -0,0 +1,14 @@
+package com.miniproxy.proxyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ProxyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/miniproxy/proxyserver/controllers/ProxyServerController.java
@@ -0,0 +1,16 @@
+package com.miniproxy.proxyserver.controllers;
+
+import org.slf4j.*;
+import org.springframework.*;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+@Component
+public class ProxyServerController {
+	private static Logger log = LoggerFactory.getLogger(ProxyServerController.class);
+	
+	/*public void request() {
+		
+	}*/
+	
+}
